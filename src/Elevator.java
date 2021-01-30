d878afa1f51a2cba77485a23c4079d8c71016f96
Omar <omarimran2000@gmail.com>
2021-01-30 02:11:13 -0500

    Added Elevator

---
+++ src/Elevator.java
@@ -0,0 +1,11 @@
+public class Elevator implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+}
