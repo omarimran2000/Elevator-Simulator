f8d96fa6752eba748166f68bb399274967e5624a
Omar <omarimran2000@gmail.com>
2021-01-31 15:21:27 -0500

    Added packages and classes

---
+++ src/ElevatorSubsystem/Elevator.java
@@ -0,0 +1,13 @@
+package ElevatorSubsystem;
+
+public class Elevator implements Runnable {
+
+    @Override
+    public void run() {
+
+    }
+    public static void main(String[] args)
+    {
+        System.out.println("Test");
+    }
+}
