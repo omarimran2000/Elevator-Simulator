fb947f31fd6e9f97c54c76c33378b4ab63f0533d
Omar <omarimran2000@gmail.com>
2021-02-01 15:07:55 -0500

    Changed CSV reader location

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-import java.io.File;
+import SchedulerSubsystem.Scheduler;
+
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner scanner = new Scanner(new File("test.csv"));
-        scanner.useDelimiter(",");
-        while(scanner.hasNext())
-        {
-            System.out.print(scanner.next());
-        }
-        scanner.close();
+        Scheduler scheduler = new Scheduler();
+        scheduler.readCSV("test.csv");
     }
 }
--- src/SchedulerSubsystem/Scheduler.java
+++ src/SchedulerSubsystem/Scheduler.java
@@ -1,6 +1,22 @@
 package SchedulerSubsystem;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Scheduler implements Runnable {
+    public Scheduler() {
+    }
+    public void readCSV(String filename) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File("test.csv"));
+        scanner.useDelimiter(",");
+        while(scanner.hasNext())
+        {
+            System.out.print(scanner.next());
+        }
+        scanner.close();
+    }
+
     @Override
     public void run() {
 
