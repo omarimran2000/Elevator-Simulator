1c7fd96f21a475529618e00411945756148f069a
Omar <omarimran2000@gmail.com>
2021-02-01 15:47:07 -0500

    Added data structure for schedule

--- src/SchedulerSubsystem/Scheduler.java
+++ src/SchedulerSubsystem/Scheduler.java
@@ -2,17 +2,33 @@ package SchedulerSubsystem;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
+import java.util.*;
 
 public class Scheduler implements Runnable {
+    ArrayList<HashMap<String,String>> schedule;
     public Scheduler() {
+        schedule = new ArrayList<HashMap<String, String>>();
     }
     public void readCSV(String filename) throws FileNotFoundException {
         Scanner scanner = new Scanner(new File("test.csv"));
-        scanner.useDelimiter(",");
+
+        String [] keys = {"time","floor","floor button","car button"};
+        int rowCount = 0;
+
         while(scanner.hasNext())
         {
-            System.out.print(scanner.next());
+            schedule.add(new HashMap<>());
+            String[] line = scanner.nextLine().split(",");
+
+            int lineCount = 0;
+
+            while(lineCount<line.length)
+            {
+                schedule.get(rowCount).put(keys[lineCount],line[lineCount]);
+                lineCount++;
+            }
+
+            rowCount++;
         }
         scanner.close();
     }
