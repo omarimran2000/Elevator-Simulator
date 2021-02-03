6dfcbe55c9c9af58f9b81f8574f69a9b49e0b9c6
Omar <omarimran2000@gmail.com>
2021-02-03 10:00:35 -0500

    Added constructors for floor and elevator

--- src/FloorSubsystem/Floor.java
+++ src/FloorSubsystem/Floor.java
@@ -8,10 +8,31 @@ import java.util.List;
 public class Floor implements Runnable {
     private final Scheduler scheduler;
     private final List<Event> schedule;
+    private FloorButton upButton;
+    private FloorButton downButton;
+    private FloorLamp upLamp;
+    private FloorLamp downLamp;
 
-    public Floor(Scheduler scheduler, List<Event> schedule) {
+    public Floor(Scheduler scheduler, List<Event> schedule,boolean topFloor,boolean bottomFloor) {
         this.scheduler = scheduler;
         this.schedule = schedule;
+        if(!topFloor && !bottomFloor){
+            upButton = new FloorButton();
+            downButton = new FloorButton();
+
+            upLamp = new FloorLamp();
+            downLamp = new FloorLamp();
+        }
+        else if(bottomFloor)
+        {
+            upButton = new FloorButton();
+            upLamp = new FloorLamp();
+        }
+        else
+        {
+            downButton = new FloorButton();
+            downLamp = new FloorLamp();
+        }
     }
 
     @Override
--- src/FloorSubsystem/FloorSubsystem.java
+++ src/FloorSubsystem/FloorSubsystem.java
@@ -19,6 +19,7 @@ public class FloorSubsystem {
 
     public FloorSubsystem(Scheduler scheduler, List<Event> schedule) {
         this.scheduler = scheduler;
+        scheduler.setSchedule(schedule);
 
         floors = new HashSet<>();
 
@@ -31,15 +32,20 @@ public class FloorSubsystem {
                 max_floor_number = floor_number;
             }
         }
-
-        for (int floor_number = 0; floor_number < max_floor_number; floor_number++) {
-            Thread temp = new Thread(new Floor(scheduler, schedule_by_floor.get(floor_number)), "Floor " + floor_number);
+        Thread temp = new Thread(new Floor(scheduler, schedule_by_floor.get(0),false,true), "Floor " + 1);
+        temp.start();
+        floors.add(temp);
+        for (int floor_number = 1; floor_number < max_floor_number-1; floor_number++) {
+            temp = new Thread(new Floor(scheduler, schedule_by_floor.get(floor_number),false,false), "Floor " + floor_number);
             temp.start();
             floors.add(temp);
         }
+        temp = new Thread(new Floor(scheduler, schedule_by_floor.get(max_floor_number-1),true,false), "Floor " + max_floor_number);
+        temp.start();
+        floors.add(temp);
     }
 
-    public FloorSubsystem(Scheduler scheduler, String schedule_filename) throws FileNotFoundException, ParseException {
+    public FloorSubsystem(Scheduler scheduler, String schedule_filename) throws FileNotFoundException, ParseException{
         this(scheduler, FloorSubsystem.readCSV(schedule_filename));
     }
 
