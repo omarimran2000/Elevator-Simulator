c8c607c3b4f82be923e6648ad6bb872c33904343
wyosief <72232976+wyosief@users.noreply.github.com>
2021-02-05 16:33:22 -0500

    Added doors opening and closing.

--- src/ElevatorSubsystem/Door.java
+++ src/ElevatorSubsystem/Door.java
@@ -1,5 +1,14 @@
 package ElevatorSubsystem;
 
 public class Door {
+    boolean isOpen;
+
+    public Door(){
+        isOpen = false;
+    }
+
+    public void setOpen(boolean isOpen){
+        this.isOpen = isOpen;
+    }
 
 }
