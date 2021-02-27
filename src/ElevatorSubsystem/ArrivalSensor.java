29ed2b11914d68a2d1d589ece83722c0883ae5ee
wyosief <72232976+wyosief@users.noreply.github.com>
2021-02-27 13:04:10 -0500

    Added delays for travelling between floors.

--- src/ElevatorSubsystem/ArrivalSensor.java
+++ src/ElevatorSubsystem/ArrivalSensor.java
@@ -10,6 +10,7 @@ public class ArrivalSensor implements Runnable {
     private final Elevator elevator;
     private boolean run;
 
+
     /**
      * Constructor for ArrivalSensor
      * Instantiates a ScheduledExecutorService
@@ -30,6 +31,11 @@ public class ArrivalSensor implements Runnable {
     public void shutDown() {
         run = false;
     }
+    public long getSecondsToTravelBetweenTwoFloors(int distance) {
+        return (long) ((distance * elevator.getConfig().getFloatProperty("distanceBetweenFloors")) / elevator.getConfig().getFloatProperty("velocity"));
+    }
+
+
 
     @Override
     public void run() {
@@ -40,10 +46,29 @@ public class ArrivalSensor implements Runnable {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            //FIXME add physics
-            if (elevator.stopForNextFloor()) {
-                elevator.atFloor();
+
+            int nextFloor;
+            if(elevator.motor.directionIsUp()){
+                 nextFloor = elevator.currentFloorNumber + 1;
+
             } else {
+                 nextFloor = elevator.currentFloorNumber - 1;
+            }
+            int distance = elevator.distanceTheFloor(nextFloor, elevator.motor.directionIsUp());
+            long seconds = getSecondsToTravelBetweenTwoFloors(distance);
+            try {
+                Thread.sleep(seconds * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("current: " + elevator.currentFloorNumber);
+            System.out.println("next: " + nextFloor);
+            if (elevator.stopForNextFloor()) {
+                System.out.println("new floor: " + elevator.currentFloorNumber);
+                elevator.atFloor();
+
+            } else {
+                 System.out.println("new floor: " + elevator.currentFloorNumber);
                 elevator.passFloor();
             }
         }
