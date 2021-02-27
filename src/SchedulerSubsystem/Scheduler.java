e5dc0cdaa046030e114a8d04c48ecba851fb9fb8
Omar <omarimran2000@gmail.com>
2021-02-27 13:23:43 -0500

    Added print statement for scheduler

--- src/SchedulerSubsystem/Scheduler.java
+++ src/SchedulerSubsystem/Scheduler.java
@@ -37,6 +37,7 @@ public class Scheduler implements Runnable {
     }
 
     public void handleFloorButton(int floorNumber, boolean isUp) {
+        System.out.println("Scheduler: scheduling event for floor "+floorNumber);
         elevators.stream()
                 .min(Comparator.comparing(elevator -> elevator.distanceTheFloor(floorNumber, isUp)))
                 .orElseThrow(NoSuchElementException::new)
