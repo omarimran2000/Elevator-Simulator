1531ce342a6e6b381a48acee293a7c2386cf12d8
Braxton Martin <braxton_martin@users.noreply.github.com>
2021-03-13 19:27:39 -0500

    Added comments/docstrings to all remaining files

--- src/ElevatorSubsystem/ElevatorApi.java
+++ src/ElevatorSubsystem/ElevatorApi.java
@@ -5,9 +5,29 @@ import model.Destination;
 import java.io.IOException;
 
 public interface ElevatorApi {
+    /**
+     * Returns the distance to the floor given the current destination of the elevator and the elevators current position
+     * @param destination
+     * @return distance between elevator and its destination
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     int distanceTheFloor(Destination destination) throws IOException, ClassNotFoundException;
 
+    /**
+     * Adds a desination for the elevator
+     * @param destination
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     void addDestination(Destination destination) throws IOException, ClassNotFoundException;
 
+    /**
+     * Checks if the elevator can add the desired destination to its queue list
+     * @param destination
+     * @return a boolean value that represents whether or not the destination can be added to its queue list
+     * @throws IOException
+     * @throws ClassNotFoundException
+     */
     boolean canAddDestination(Destination destination) throws IOException, ClassNotFoundException;
 }
